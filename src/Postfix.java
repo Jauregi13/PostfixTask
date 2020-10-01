1f1f59a41703d14bfb6b20a8fa2818caeb772bd3
jauregi13 <jon.j1305@gmail.com>
2020-10-01 14:00:03 +0200

    First Commit

---
+++ src/Postfix.java
@@ -0,0 +1,55 @@
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Postfix    
+{
+  /**
+   * Reads and evaluates multiple postfix expressions.
+   */
+  public static void main (String[] args)
+  {
+    String expression;
+    int result;
+    
+    // input file
+    String path = "/home.... les/postfixexpressions.txt";
+	File fname = new File( path );
+	
+	//output file
+	String write2file = "/hom....afiles/postfixResults.txt";
+	File wrname = new File (write2file);
+	
+	try{
+
+		Scanner input2program = new Scanner (fname);
+		PrintWriter wrfile = new PrintWriter (wrname);
+        PostfixEvaluator evaluator = new PostfixEvaluator();
+		
+		while ( input2program.hasNext() ){		
+			expression = input2program.nextLine();
+			
+	        result = evaluator.evaluate (expression);
+	        
+			wrfile.println(expression);
+			wrfile.println("----------> "+ result);
+		}
+		
+		input2program.close();
+		wrfile.close();
+	}
+	catch ( FileNotFoundException e ) {
+			 e.printStackTrace();
+	}
+    
+    
+    
+    
+    
+    
+    
+  }
+}
